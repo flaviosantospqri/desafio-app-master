b46379f99e5339f4d482191f3afe96529677687b
Flavio Santos <flavio.pasantos@outlook.com>
2023-07-09 21:48:45 -0300

    feat: Create signin feature

--- src/components/formLogin/index.jsx
+++ src/components/formLogin/index.jsx
@@ -1,22 +1,68 @@
-import { Link } from "react-router-dom";
-import { Container } from "./style";
+import { Link, useNavigate } from "react-router-dom";
+import { Container } from "../formLogin/style";
+import { useState } from "react";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { db } from "../../services/firebase";
+import { toast } from "react-toastify";
 
-function FormLogin() {
+function FormRegister() {
+  const [dataUser, setDataUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = dataUser;
+
+  const navigate = useNavigate();
+
+  function onChange(e) {
+    setDataUser((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+        toast.success("Login was successful");
+      }
+    } catch (error) {
+      toast.error("Error on login certify your informations is correct");
+    }
+  }
   return (
     <Container>
       <Link to="/">Back</Link>
       <div className="container">
         <div className="container_form">
-          <form className="container_form_items">
+          <form className="container_form_items" onSubmit={onSubmit}>
             <h1 className="text-title">Login</h1>
+
             <div className="field-group">
               <label className="label" htmlFor="txt-email">
                 Email address
               </label>
               <input
                 className="input"
+                value={email}
+                onChange={onChange}
                 type="email"
-                id="txt-email"
+                id="email"
                 name="email"
                 placeholder="Email"
               />
@@ -28,21 +74,23 @@ function FormLogin() {
               <input
                 className="input"
                 type="password"
-                id="txt-password"
+                value={password}
+                onChange={onChange}
+                id="password"
                 name="password"
                 placeholder="*********"
               />
+              <Link to="/register" className="Enter">
+                Enter
+              </Link>
             </div>
             <div className="field-group">
               <input className="btn-submit" type="submit" value="Log In" />
             </div>
-            <Link to="/register" className="link-forgot">
-              Forgot?
-            </Link>
           </form>
         </div>
       </div>
     </Container>
   );
 }
-export default FormLogin;
+export default FormRegister;
